083037792b8527639715703217758ec032bbfacd
Giliard Godoi <giliard.godoi@gmail.com>
2024-03-21 22:26:07 -0300

    add pt-core-news-lg as extras

--- src/tjdatasets/__init__.py
+++ src/tjdatasets/__init__.py
@@ -4,6 +4,3 @@ import spacy
 nltk.download('punkt')
 nltk.download('rslp')
 nltk.download('stopwords')
-
-if not any(model.startswith('pt') for model in spacy.util.get_installed_models()):
-        spacy.cli.download('pt_core_news_lg')
