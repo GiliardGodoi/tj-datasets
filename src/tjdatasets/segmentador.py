2eb75c47a074d423a4f6b6c1de2ea8d96b0dd098
Giliard Godoi <giliard.godoi@gmail.com>
2024-04-10 21:29:33 -0300

    add Segmentador class

--- src/tjdatasets/segmentador.py
+++ src/tjdatasets/segmentador.py
@@ -1,3 +1,9 @@
+
+import pandas as pd
+import re
+
+from collections import defaultdict
+from sklearn.base import BaseEstimator, TransformerMixin
 from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktParameters
 
 abbreviations = {
@@ -86,3 +92,58 @@ _tokenizer = PunktSentenceTokenizer(punkt_param)
 
 def sentence_tokenize(text):
     return _tokenizer.tokenize(text)
+
+
+
+class Segmentador(BaseEstimator, TransformerMixin):
+    '''
+    '''
+    def __init__(self,
+                 segment_name=None,
+                 sentence_sep=' ',
+                 safe_dtypes=False):
+        
+        assert segment_name in SEGMENT_EXPRESSIONS.keys(), f'O nome do segmento precisa ser um dos seguintes: lei, fato, decisao, pedido'
+
+        self.segment_name = segment_name
+        self.safe_dtypes = safe_dtypes
+        self.column_sentence = f"segmento_{segment_name}"
+        self.sentence_sep = sentence_sep
+        self.mapping_dtypes = defaultdict(lambda : 'object',  # default
+                     numero_processo='category',
+                     id_documento='category',
+                     conteudo='string',
+                     formatado='string',
+                     codigos_movimentos_temas='string')
+
+    def fit(self, X):
+        return self
+
+    def transform(self, df):
+        column_sentence = self.column_sentence
+        if self.safe_dtypes:
+            columns_dtypes = {column: df[column].dtype for column in df.columns}
+            columns_dtypes.update(self.mapping_dtypes)
+        else:
+            columns_dtypes = self.mapping_dtypes
+        # change columns dtype for optimization purposes
+        df = df.astype(columns_dtypes)
+
+        frame = df[['numero_processo', 'id_documento']].copy()
+        frame[column_sentence] = df['formatado'].apply(sentence_tokenize)
+        frame = frame.explode(column_sentence)
+        frame['contains'] = False
+        # for segment in SEGMENT_EXPRESSIONS.keys():
+        #     frame['contains'] = False
+
+        for expression in SEGMENT_EXPRESSIONS[self.segment_name]:
+            frame['contains'] = frame['contains'] | frame[column_sentence].str.contains(expression, regex=True, flags=re.I)
+        
+        frame = (frame
+                    .groupby(['contains', 'numero_processo'])
+                    .agg({column_sentence: lambda values: f'{self.sentence_sep}'.join(values)})
+                    .loc[True] )
+        
+        frame = pd.merge( df['numero_processo'], frame, how='left', left_on='numero_processo', right_index=True)
+
+        return frame
